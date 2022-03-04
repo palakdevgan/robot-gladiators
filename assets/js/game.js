8300959084321db936557bb60bdabd711d3d3bb5
palakdevgan <m7.palak@gmail.com>
2022-03-04 12:29:41 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -56,16 +56,48 @@ var fight = function(enemyName) {
     
 };
 
-
+var startGame=function(){
 for(var i=0;i < enemyNames.length;i++){
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
     if (playerHealth > 0) {
     window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ));
     var pickedEnemyName = enemyNames[i];
     enemyHealth=50;
     fight(pickedEnemyName);
 }
+
 else {
     window.alert("You have lost your robot in battle! Game Over!");
     break;
   }
 }
+endGame();
+ // play again
+ startGame();
+};
+
+// function to end the entire game
+var endGame = function() {
+    // if player is still alive, player wins!
+  if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+  } 
+  else {
+    window.alert("You've lost your robot in battle.");
+  }
+  // ask player if they'd like to play again
+var playAgainConfirm = window.confirm("Would you like to play again?");
+
+if (playAgainConfirm) {
+  // restart the game
+  startGame();
+} 
+else {
+  window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+}
+  };
+
+// start the game when the page loads
+startGame();
