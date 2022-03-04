fc5d02880ccaef76eb0e0fde6d118cf8c3479e82
palakdevgan <m7.palak@gmail.com>
2022-03-04 09:38:43 -0500

    new enemies added

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,7 +8,8 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
-     window.alert("Welcome to Robot Gladiators!");
+
+    while(enemyHealth > 0){
 
      var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -51,9 +52,12 @@ var fight = function(enemyName) {
     }
     else {
         window.alert("You need to choose a valid option. Try again!");
-      }  
+      } 
+    } 
 };
 
 for(var i=0;i < enemyNames.length;i++){
-fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth=50;
+    fight(pickedEnemyName);
 }
